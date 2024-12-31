917ed9df0fd7a93aa1e66c0a38d29d4af181ba13
Carol Leiva N <ca.leivan@duocuc.cl>
2024-12-31 19:29:50 -0300

    feat: creacion de carpeta Desarrollo Backend Python.py

---
+++ Clase 1 Intro avanzada en Python/Clase Bloque A.py
@@ -0,0 +1,30 @@
+"""String, busca el valor"""
+
+nombre = "Talento fututo"
+print(nombre[0]) # imprime T
+
+
+
+"""Strings con dos variables y concatenado con el operador +. """
+
+palabra_uno = "Talento"
+palabra_dos = "Futuro"
+print(palabra_uno + " " + palabra_dos) # imprime “Talento Futuro”
+
+
+"""String con operador Slice, usando los corchetes cuadrados 
+indicando el inicio y el final."""
+
+nombre = "Talento Futuro"
+print(nombre[0:7]) #imprime "Talento"
+
+"""si queremos invertir un string"""
+nombre = "Talento Futuro"
+print(nombre[::-1]) # imprime “orutuF otnelaT”
+
+"""Entrada y salidas"""
+edad = int(input("Ingrese edad: "))
+print("La edad es: ", edad)    # imprime la edad es :xx
+ 
+edad = int(input("Ingrese edad: "))
+print("La edad es: " + edad)
